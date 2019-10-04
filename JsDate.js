51f49a12a1856b635c1fba236293bc30fab56ba3
MaksimYakutin <aksimax7@gmail.com>
2019-10-04 13:07:37 +0700

    linux git

--- JsDate.js
+++ JsDate.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function dateHomePage(){
     let Mytime = document.getElementById("dateOfBirth").value;
    
@@ -18,24 +17,4 @@ function dateHomePage(){
     }
     
 }  
-=======
-function dateHomePage(){
-    let Mytime = document.getElementById("dateOfBirth").value;
-   
-    if(!Mytime){
-        document.getElementById("dOB").innerHTML = 'Пустое поле Имя';
-    }else{
-        let date = new Date(Mytime);
-        let dateMy = Date.parse(date);
-        let Today = Date.now();
-        let age =(Today - dateMy) / (365*24*60*60*1000); 
-        if(Math.floor(age) < 18){
-            document.getElementById("dOB").innerHTML = 'Возрост меньше 18';
-		}else {
-            document.getElementById("dOB").innerHTML = 'Возрост больше';
-        
-        }
-    }
-    
-}  
->>>>>>> 3b653c743e68584aa014be05a49bf778c10e9b8d
+
